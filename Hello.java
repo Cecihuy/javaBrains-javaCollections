6ecc5d530a48dfc0af4f2105aad7f284b172779f
sourceTree <sourcetree@mail.com>
2024-01-01 21:45:37 -0700

    19 hashCode in Java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ class Hello{
         Student student1 = new Student("foo", "bar", 1, "science");
         Student student2 = new Student("foo", "bar", 1, "science");
 
-        System.out.println(student1 == student2);
-        System.out.println(student1.equals(student2));
+        System.out.println(student1.hashCode());
+        System.out.println(student2.hashCode());
     }    
 }
